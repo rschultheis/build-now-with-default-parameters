2fa193089d766bbdf1bf0268938755a26e922cce
rob <robert.schultheis@gmail.com>
2015-04-16 18:05:08 -0400

    fully working column button

---
+++ src/main/java/org/jenkins/ci/plugins/BuildNowListViewColumnDecorator.java
@@ -0,0 +1,13 @@
+package org.jenkins.ci.plugins;
+
+
+import hudson.Extension;
+import hudson.model.PageDecorator;
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.StaplerRequest;
+
+@Extension
+public class BuildNowListViewColumnDecorator extends PageDecorator {
+
+}
