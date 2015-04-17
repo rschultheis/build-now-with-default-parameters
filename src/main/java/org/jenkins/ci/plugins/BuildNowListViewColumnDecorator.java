e34503c45fe2c2f095f945d18ee5e17226ffc431
rob <robert.schultheis@gmail.com>
2015-04-17 12:33:04 -0400

    changed behavior to not redirect
    if build successfully launches, then change the icon
      instead of redirecting so user can click many builds off quickly

--- src/main/java/org/jenkins/ci/plugins/BuildNowListViewColumnDecorator.java
+++ src/main/java/org/jenkins/ci/plugins/BuildNowListViewColumnDecorator.java
@@ -7,6 +7,7 @@ import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.StaplerRequest;
 
+// This class causes the main page load to include the custom javascript in header.jelly
 @Extension
 public class BuildNowListViewColumnDecorator extends PageDecorator {
 
